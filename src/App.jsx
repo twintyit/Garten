fefa31b2a04374a2d153620e451115b870461367
Kamyshov Ruslan <kamyshov02u@gmail.com>
2025-01-30 00:22:13 +0100

    domen-3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import {Home} from "./pages/home-page/HomePage.jsx";
 
@@ -6,6 +5,7 @@ function App() {
 
   return (
     <>
+        <div>Hello world</div>
         <Home></Home>
     </>
   )
