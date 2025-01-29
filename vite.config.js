aeb33b861bac0f416be9bd62c738ec5c2ccc141d
Kamyshov Ruslan <kamyshov02u@gmail.com>
2025-01-30 00:05:29 +0100

    domen-2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "CNAME",
+          dest: ".", // Копировать в корень dist
+        },
+      ],
+    }),
+  ],
+});
