3de7d6e4801c27ad0172ce6e319c7ae67169ece3
Kamyshov Ruslan <kamyshov02u@gmail.com>
2025-01-29 22:34:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import {Home} from "./pages/home-page/HomePage.jsx";
+
+function App() {
+
+  return (
+    <>
+        <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home-page/HomePage.jsx
@@ -0,0 +1,6 @@
+export const Home = () => {
+    return <div className="container">
+        <h1>Главная</h1>
+        <p>Информация об услугах и ценах.</p>
+    </div>;
+}
